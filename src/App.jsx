4d0abd2e90c887771037c9fdf8cfed8ef2bfbe4c
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-08-04 19:30:00 +0530

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="w-full">
+      <header className="flex justify-center items-center font-bold text-2xl">
+        NSS EMEA Website
+      </header>
+    </div>
+  );
+}
+
+export default App;
