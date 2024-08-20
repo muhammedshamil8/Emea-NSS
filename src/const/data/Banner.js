cdf152bd6ce21f252555cc952eb013ac83fe418d
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-08-20 16:41:39 +0530

    chore: add constants and data files

---
+++ src/const/data/Banner.js
@@ -0,0 +1,15 @@
+import Banner1 from '../../assets/images/Banner/banner1.png';
+import Banner2 from '../../assets/images/Banner/banner3.jpeg';
+
+const BannerIMG = [
+    {
+        src: Banner1,
+        alt: 'Banner 1',
+    },
+    {
+        src: Banner2,
+        alt: 'Banner 2',
+    },
+];
+
+export default BannerIMG;
---
+++ src/const/navLinks.ts
@@ -0,0 +1,24 @@
+const navLinks = [
+  {
+    label: "Home",
+    href:"/#home"
+  },
+  {
+    label: "Activities",
+    href:"/activities"
+  },
+  {
+    label: "Gallery",
+    href:"/gallery"
+  },
+  {
+    label: "Reports",
+    href:"/reports"
+  },
+  {
+    label: "Contact Us",
+    href:"/#contact"
+  }
+];
+
+export default navLinks;
