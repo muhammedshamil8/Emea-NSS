adbebe2be12dfdc3099664384f970f90f75fa7f3
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-09-25 15:24:43 +0530

    fix: update firebase configuration

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,8 +16,11 @@ const firebaseConfig = {
   appId: `${import.meta.env.VITE_FIREBASE_APP_ID}`
 };
 
-console.log(firebaseConfig);
+// console.log(firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+
+export const db = getFirestore(app);
+export const storage = getStorage(app);
