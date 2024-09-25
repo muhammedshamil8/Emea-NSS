7eb6767d8d49a8a32f5766b655355cafde8cadd0
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-09-25 22:23:38 +0530

    layout: update GuestLayout and About page

--- src/layout/GuestLayout.jsx
+++ src/layout/GuestLayout.jsx
@@ -4,14 +4,14 @@ import { NavBar, Footer } from "../components/layout";
 
 function GuestLayout() {
     return (
-        <>
-            <div className="fixed !z-50 left-0 right-0 h-16 mb top-0 bg-white shadow-sm shadow-gray-400">
+        <main className='w-full'>
+            <div className="fixed !z-50 left-0 w-full right-0 h-16  top-0 bg-white shadow-sm shadow-gray-400 max-w-full">
                 <NavBar />
             </div>
             <div className="mt-16" />
             <Outlet />
             <Footer />
-        </>
+        </main>
     )
 }
 
--- src/pages/General/About/index.jsx
+++ src/pages/General/About/index.jsx
@@ -9,7 +9,7 @@ function About() {
         <p className='text-lg text-center max-w-[800px] mx-auto '>The National Service Scheme (NSS)</p>
       </div>
       <div className='flex flex-col'>
-        <section className='py-4 flex'>
+        <section className='py-4 flex flex-col md:flex-row'>
           <div className='flex-1 flex items-start flex-col p-4'>
             <h1 className='text-2xl text-center text-indigo-700 font-semibold'>Our Vision</h1>
             <p className='text-lg text-left max-w-[800px] mx-auto mt-4'>The National Service Scheme (NSS) has been a part of the Indian ethos and has had immense impact on the development of the student youth through the decades. The very first idea of such a program was mooted by the then Union Education Minister Dr. V.K.R.V. Rao in 1969. It was aimed at developing in the students a sense of participation in nation building as well as inculcating in them such values and norms as would help them to become meaningful citizens of a democratic society.</p>
@@ -18,7 +18,7 @@ function About() {
             <img src={Event1} alt='Event 1' className='rounded-xl object-cover h-auto w-auto' />
           </div>
         </section>
-        <section className='py-4 flex'>
+        <section className='py-4 flex flex-col-reverse md:flex-row'>
           <div className='flex-1 '>
             <img src={Event1} alt='Event 1' className='rounded-xl object-cover h-auto w-auto' />
           </div>
@@ -27,7 +27,7 @@ function About() {
             <p className='text-lg text-left max-w-[800px] mx-auto mt-4'>The National Service Scheme (NSS) has been a part of the Indian ethos and has had immense impact on the development of the student youth through the decades. The very first idea of such a program was mooted by the then Union Education Minister Dr. V.K.R.V. Rao in 1969. It was aimed at developing in the students a sense of participation in nation building as well as inculcating in them such values and norms as would help them to become meaningful citizens of a democratic society</p>
           </div>
         </section>
-        <section className='py-4 flex'>
+        <section className='py-4 flex flex-col md:flex-row'>
           <div className='flex-1 flex items-start flex-col p-4'>
             <h1 className='text-2xl text-center text-indigo-700 font-semibold'>Our Approach</h1>
             <p className='text-lg text-left max-w-[800px] mx-auto mt-4'>The National Service Scheme (NSS) has been a part of the Indian ethos and has had immense impact on the development of the student youth through the decades. The very first idea of such a program was mooted by the then Union Education Minister Dr. V.K.R.V. Rao in 1969. It was aimed at developing in the students a sense of participation in nation building as well as inculcating in them such values and norms as would help them to become meaningful citizens of a democratic society.</p>
