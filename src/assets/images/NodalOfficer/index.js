6e3677ac5059d88666ae444071709052920eb4fd
muhammedshamil8 <muhammedshamil008@gmail.com>
2025-01-13 14:43:28 +0530

    updated nodal officer section

--- src/assets/images/NodalOfficer/index.js
+++ src/assets/images/NodalOfficer/index.js
@@ -1,4 +1,5 @@
 import JasimSir from './jazim_sir.png';
 import ShihabudheenSir from './Shihabudheen_sir.png';
+import JadimSirBig from './munavar_jazim.png';
 
-export  { JasimSir, ShihabudheenSir };
+export  { JasimSir, ShihabudheenSir ,JadimSirBig};
